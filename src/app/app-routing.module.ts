0f9f85d9a05dba1a62031964353c31917b28241d
NewGrafon <ultrafig73@gmail.com>
2023-04-26 18:59:32 +0400

    some фичес

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AccountRegistrationComponent } from "./pages/account-registration/account-registration.component";
 import { AccountLoginComponent } from "./pages/account-login/account-login.component";
 import { HomeComponent } from "./pages/home/home.component";
+import {AccountProfileComponent} from "./pages/account-profile/account-profile.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'registration', component: AccountRegistrationComponent },
-  { path: 'login', component: AccountLoginComponent }
+  { path: 'login', component: AccountLoginComponent },
+  { path: 'profile', component: AccountProfileComponent }
 ];
 
 @NgModule({

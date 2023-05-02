c2b0c49ee26f1661599ab6cc882de7259ad597c6
NewGrafon <ultrafig73@gmail.com>
2023-05-02 19:44:23 +0400

    some фичес and фиксес

--- src/assets/scripts/popup.js
+++ src/assets/scripts/popup.js
@@ -1,29 +1,19 @@
+function openPopup(popupClassName = '', popupBGName = '') {
+  if (popupClassName === '' || popupBGName === '') {
+    console.error("Не указано чета")
+  }
 
+  document.querySelector(`.${popupClassName}`).classList.add('active')
+  document.querySelector(`.${popupBGName}`).classList.add('active')
+}
 
-let popupBg = document.querySelector('.popup-bg')
-let popup = document.querySelector('.popUpteacher')
-let openPopupButton = document.querySelector('.openPopup')
-let closePopupButton = document.querySelector('.closePopup')
+function closePopup(popupClassName = '', popupBGName = '') {
+  if (popupClassName === '' || popupBGName === '') {
+    console.error("Не указано чета")
+  }
 
+  document.querySelector(`.${popupClassName}`).classList.remove('active')
+  document.querySelector(`.${popupBGName}`).classList.remove('active')
+}
 
-openPopupButton.forEach((button) => { // Перебираем все кнопки
-    button.addEventListener('click', (e) => { // Для каждой вешаем обработчик событий на клик
-        e.preventDefault(); // Предотвращаем дефолтное поведение браузера
-        popupBg.classList.add('active'); // Добавляем класс 'active' для фона
-        popup.classList.add('active'); // И для самого окна
-    })
-});
-
-
-closePopupButton.addEventListener('click',() => { // Вешаем обработчик на крестик
-    popupBg.classList.remove('active'); // Убираем активный класс с фона
-    popup.classList.remove('active'); // И с окна
-});
-
-document.addEventListener('click', (e) => { // Вешаем обработчик на весь документ
-    if(e.target === popupBg) { // Если цель клика - фот, то:
-        popupBg.classList.remove('active'); // Убираем активный класс с фона
-        popup.classList.remove('active'); // И с окна
-    }
-});
 

595731555d07ab67a3a3c2826fc8a0df08ea88a6
NewGrafon <ultrafig73@gmail.com>
2023-05-04 16:07:20 +0400

    some fixes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  frontendUrl: 'http://89.223.65.121',
-  apiUrl: 'http://89.223.65.121/api'
+  frontendUrl: 'http://46.229.212.145',
+  apiUrl: 'http://46.229.212.145/api'
 };
